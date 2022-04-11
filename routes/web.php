f7b4dee2c510d2d515afe7e8421b249efe06d517
kamal ait omar <aitomarkamal@gmail.com>
2022-04-11 17:50:08 +0000

    start working on invoices page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -23,5 +25,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('invoices', InvoicesController::class);
 
 Route::get('/{page}', [AdminController::class, 'index']);
