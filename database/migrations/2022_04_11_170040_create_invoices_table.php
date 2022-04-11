bf075b1a201d80ea65dcf719a3b474bc0fb7280a
kamal ait omar <aitomarkamal@gmail.com>
2022-04-11 17:22:36 +0000

    table migrated

--- database/migrations/2022_04_11_170040_create_invoices_table.php
+++ database/migrations/2022_04_11_170040_create_invoices_table.php
@@ -14,7 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('invoices', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('invoice_number');
+            $table->date('invoice_Date');
+            $table->date('due_Date');
+            $table->string('product');
+            $table->string('section');
+            $table->string('discount');
+            $table->string('rate_vat');
+            $table->decimal('value_vat', 8, 2);
+            $table->decimal('total', 8, 2);
+            $table->string('status', 50);
+            $table->integer('value_status');
+            $table->text('note')->nullable();
+            $table->string('user');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
